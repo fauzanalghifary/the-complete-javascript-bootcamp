f0a13b03cdff8e293af9d1dc5b388afd9884b686
Fauzan Al Ghifary <m.fauzanghifary@gmail.com>
2022-03-26 12:43:52 +0700

    continue section 2 - JS Fundamentals Part 1

---
+++ Section 02 Javascript Fundamentals Part 1/script.js
@@ -0,0 +1,40 @@
+// console.log("hi");
+// console.log(23);
+
+//--------------------------------------- Lecture 10 Values and Variable
+
+// let firstName = 'Fauzan';
+// console.log(firstName);
+
+// Use camel case for variable name (camelCase)
+// Don't start variable with number
+// Variable name can only contain letters, numbers, underscores, and dollar sign
+// Can't use reserve name (new, name, etc)
+// Don't start variable name with uppercase letter
+// ALL uppercase if the variable value is constant
+// VARIABLE NAME SHOULD BE DESCRIPTIVE!
+
+// --------------------------------------- Lecture 11 Data Types
+
+// console.log(typeof 23);
+
+// let year;
+// console.log(typeof year);
+// year = 1997;
+// console.log(typeof year);
+
+// --------------------------------------- LET, CONST, VAR
+
+// let age = 30;
+// age = 31;
+// const birthYear = 1991;
+// birthYear = 1991 THIS WILL PRODUCE ERROR
+
+// --------------------------------------- BASIC OPERATOR
+
+const now = 2022;
+const ageFauzan = now - 1997;
+const ageElmia = now - 1998;
+console.log(ageFauzan, ageElmia);
+
+
