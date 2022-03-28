bfac0219cbebce28b96f53c18e6f6586f05271f6
Fauzan Al Ghifary <m.fauzanghifary@gmail.com>
2022-03-28 07:54:39 +0700

    continue section 07

--- Section 07 DOM Fundamentals/05-Guess-My-Number/script.js
+++ Section 07 DOM Fundamentals/05-Guess-My-Number/script.js
@@ -1,1 +1,71 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Hehe';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 200;
+
+// document.querySelector('.guess').value = 20;
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+console.log(secretNumber);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess, guess);
+  document.querySelector('.guess').value = '';
+
+  // When there is no input
+  if (!guess) {
+    displayMessage('⛔ No number!');
+  }
+
+  // When player win
+  else if (guess === secretNumber) {
+    displayMessage('✨CORRECT!');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.check').disabled = true;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  }
+
+  // When input wrong
+  else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too High!' : 'Too Low!');
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('You Lost!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  console.log(secretNumber);
+
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = '#222';
+
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('.check').disabled = false;
+});
