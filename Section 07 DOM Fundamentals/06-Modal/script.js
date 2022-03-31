97b6938b5522b7b44232b07935e4ec00c7bdd919
Fauzan Al Ghifary <m.fauzanghifary@gmail.com>
2022-03-31 07:54:55 +0700

    finish section 7 - DOM Fundamentals

--- Section 07 DOM Fundamentals/06-Modal/script.js
+++ Section 07 DOM Fundamentals/06-Modal/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  //   e.key === 'Escape' ? closeModal() : null;
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+
+  console.log(e.key);
+});
